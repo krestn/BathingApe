921c411b3ad3609552f06619ce01ba7663acb45f
Kreston <kreston.cm@gmail.com>
2022-02-15 23:21:54 -0600

    Splash initial setup

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,6 +1,7 @@
 
-import './Splash.css'
-
+import './Splash.css';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Splash() {
   return (
@@ -8,16 +9,52 @@ function Splash() {
 
     <div className='splashPage'>
       <div className='leftsideSplash'>
-        <div className='iphone'>
-          <img src='/images/iphone.png' alt="iPhone" />
-        </div>
+          <img className='phone' src='/images/iphone.png' alt="iPhone" />
+          <img className='bathing' src='/images/bathing.png' alt="bape" />
+          <img className='mamo' src='/images/mamo.png' alt="mamo" />
+
+
 
       </div>
       <div className='rightSideSplash'>
         <div className='splashLayout'>
-          <div className='splashForm'>
-            <img src='/images/bape.png' alt='Bathing Ape'/>
+          <div className='splashFormContainer'>
+            <img className="bape"src='/images/bape.png' alt='Bathing Ape'/>
+            <form className='splashForm'>
+              <input type="text" name="name" placeholder='Phone number, username, or email' />
+              <input type="text" name="name" placeholder='Password' />
 
+              <input type="submit" value="Log In" />
+            </form>
+            <div className='divider'>
+              {/* <div className='line'></div> */}
+              <p className='or'>  OR  </p>
+              {/* <div className='line'></div> */}
+
+            </div>
+            <div>
+              <button>Log in with Demo</button>
+            </div>
+
+
+
+
+          </div>
+
+          <div className='signUpSplash'>
+            <div className='signUpContainer'>
+
+            <div>Don't have an account? 
+            </div>
+
+            <NavLink to='/signup'>{` Sign up`}</NavLink>
+            </div>
+          </div>
+
+          <p>Get the app.</p>
+          <div>
+            <img className='splashButton' src='/images/button1.png' alt='button' />
+            <img className='splashButton' src='/images/button2.png' alt='button' />
 
           </div>
 
