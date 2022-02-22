22e3e158432c6d097cbaaf8b4e369e1d030eb654
krestn <allahwolve@gmail.com>
2022-02-22 11:16:24 -0600

    Forms updated

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,6 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+from .create_post_form import CreatePostForm
+from .edit_post_form import EditPostForm
+from .create_comment_form import CreateCommentForm
+from .edit_comment_form import EditCommentForm
