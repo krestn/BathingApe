1f04f63e7776f1d768922d9fb1ae01d5a372b658
krestn <allahwolve@gmail.com>
2022-02-22 11:40:27 -0600

    updated seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,7 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-from .images import seed_images, undo_images
-from .likes import seed_likes, undo_likes
+# from .images import seed_images, undo_images
+# from .likes import seed_likes, undo_likes
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,8 +12,8 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
-    seed_images()
-    seed_likes()
+    # seed_images()
+    # seed_likes()
     # Add other seed functions here
 
 
@@ -21,6 +21,6 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
-    undo_images()
-    undo_likes()
+    # undo_images()
+    # undo_likes()
     # Add other undo functions here
